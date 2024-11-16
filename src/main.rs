b15267b68b427ce577de21b00a48782a937f48cd
Rom Rakharb <rom.rakharb@mailfence.com>
2024-11-16 16:06:43 +0700

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use maud::{html, Markup};
+
+async fn hello_world() -> Markup {
+    html! {
+        h1 { "Hello World" }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
