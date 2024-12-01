7544c822d159264db8b8716cb66f72b4fb849b13
Rom Rakharb <121065878+RomRakharb@users.noreply.github.com>
2024-12-01 11:46:09 +0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Rust Template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rustc
+        cargo
+        clippy
+        rustfmt
+        rust-analyzer
+      ];
+
+
+      shellHook = ''
+        echo -e '[language-server.rust-analyzer.config]\ncheck.command = "clippy"' > ~/.config/helix/languages.toml
+        clear
+        echo "Rust Development Environment"
+        rustc --version
+        cargo --version
+      '';
+    };
+  };
+}
